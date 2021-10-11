ac0b79c9325d117059a08bc9e1b810967bd2f7ec
Yash Devadiga <yashsdeva@gmail.com>
2021-10-11 09:45:45 +0530

    Payment button works

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
 
-      <Stack.Navigator initialRouteName='adminOrderScreen'>
+      <Stack.Navigator initialRouteName='FeedBack'>
         <Stack.Screen name='Admin Login' component={AdminLogin} options={{ headerShown: false }} />
         <Stack.Screen name='Admin Register' component={AdminRegister} options={{ headerShown: false }} />
         <Stack.Screen name='adminOrderScreen' component={adminOrderScreen} options={{ headerShown: false }} />
--- Components/FeedBack.js
+++ Components/FeedBack.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Dimensions, ImageBackground, TouchableOpacity, } from 'react-native';
 
+import fire from './firebase';
+import 'firebase/database'
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('screen').height;
 
 
-export default function FeedBack({navigation}){
 
+
+export default function FeedBack({navigation}){
+    //const [orderNo, setorderNo] = React.useState(route.params ? route.params : null);
+    const [orderNo, setorderNo] = React.useState(2);
+    const [keypar,setKey] = React.useState()
+    React.useEffect(() => {
+    fire.database().ref('Orders').orderByChild('OrderNo').equalTo(orderNo).on('value',snap=>{
+        snap.forEach((child=>{setKey(child.key)
+        }))        
+    })
+}, []);
+
+console.log(keypar)
     return(
         <View style={{flex: 1}}>
             <ImageBackground source={require('../assets/background.png')} style={styles.bgimage}>
                 <TouchableOpacity style={styles.backarrow} onPress={() => navigation.navigate("admincart")}>
                     <Image source={require('../assets/backarrow.png')} style={{resizeMode:'contain', width: '100%', height: '100%'}} />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.paymentbtn} onPress={() => navigation.navigate("adminOrderScreen")}>
+                <TouchableOpacity style={styles.paymentbtn} onPress={() => {
+                    fire.database().ref("Orders/"+keypar).update({
+                        Status: "R"
+                    })
+                    navigation.navigate("adminOrderScreen")}}>
                     <Image source={require('../assets/Paymentbtn.png')} style={{resizeMode: 'contain', width: '100%', height: '100%'}} />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.orderaccept} onPress={() => navigation.navigate("adminOrderScreen")}>
